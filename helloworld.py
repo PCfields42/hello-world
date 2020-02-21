8aa7687acaf5cf3c30867041c2fc99ab4b6a6aee
Patrick Fields <patrickfields42@gmail.com>
2020-02-20 21:17:26 -0500

    python stuff!!!

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+def addtwo(x):
+    return x + 2
+
+z = addtwo(7)
+print(z)
+
+def addnumbers(x, y):
+    return x + y
+
+q = addnumbers(5, 8)
+print("q = ", q)
+
+t = addnumbers(500, 30)
+print(t)
